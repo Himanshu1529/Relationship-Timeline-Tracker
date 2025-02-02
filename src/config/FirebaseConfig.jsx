62aa87223a06e0cdaac027cc4ce3b048494ff56a
Himanshu Sharma <himanshu291003@gmail.com>
2025-02-02 10:54:45 +0530

    Update FirebaseConfig.jsx

--- src/config/FirebaseConfig.jsx
+++ src/config/FirebaseConfig.jsx
@@ -1,19 +1,16 @@
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration from .env file
 const firebaseConfig = {
-  apiKey: "AIzaSyCAKM9aA4VB713avqSuYsFQ-SK8RAtpFLY",
-  authDomain: "friends-timeline.firebaseapp.com",
-  projectId: "friends-timeline",
-  storageBucket: "friends-timeline.firebasestorage.app",
-  messagingSenderId: "752576559569",
-  appId: "1:752576559569:web:0d50d8612df2453f4b08f0",
-  measurementId: "G-50ZR2KSMPG",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
